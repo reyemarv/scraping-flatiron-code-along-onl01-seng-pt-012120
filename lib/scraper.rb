d8f955ce7cd7341839c06d9e44a9c10bb6db9f57
Marvin Reyes <reyemarv@gmail.com>
2020-03-10 01:07:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,14 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
     
-    binding.pry 
+    doc.css(".post").each do |post|
+      course = Course.new 
+      course.title = post.css("h2").text 
+      course.schedule = post.css(".date").text 
+      course.descrption = post.css("p").text 
+      
+      binding.pry 
+    end 
   end 
   
 end
